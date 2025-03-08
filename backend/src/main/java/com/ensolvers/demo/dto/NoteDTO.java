1ac9a745639baa63b38dbd4d6bff76aa50e8d93d
matucson1 <115057949+matuteFanDeWandaNara@users.noreply.github.com>
2025-03-07 22:53:14 -0300

    feat(back) NoteDTO && NoteMapper

--- backend/src/main/java/com/ensolvers/demo/dto/NoteDTO.java
+++ backend/src/main/java/com/ensolvers/demo/dto/NoteDTO.java
@@ -1,5 +1,30 @@
 package com.ensolvers.demo.dto;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class NoteDTO {
+	
+	@NotNull
+	private Long id;
+	
+	@NotNull(message= "title cannot be null")
+	private String title;
+	
+	@NotNull(message= "text cannot be null")
+	private String text;
+	
+	@NotNull
+	private Long userid;
+	
+	private Long categoryid;
 
 }
--- backend/src/main/java/com/ensolvers/demo/mapper/NoteMapper.java
+++ backend/src/main/java/com/ensolvers/demo/mapper/NoteMapper.java
@@ -1,5 +1,42 @@
 package com.ensolvers.demo.mapper;
 
+import com.ensolvers.demo.dto.NoteDTO;
+import com.ensolvers.demo.model.Category;
+import com.ensolvers.demo.model.Note;
+import com.ensolvers.demo.model.User;
+
 public class NoteMapper {
 
+	public static NoteDTO toDTO(Note note) {
+		return NoteDTO.builder()
+				.id(note.getId())
+				.title(note.getTitle())
+				.text(note.getText())
+				.userid(note.getUser().getId())
+				.categoryid(note.getCategory().getId())
+				.build();
+	}
+	
+	public static Note toNewEntity(NoteDTO noteDTO,User user, Category category) {
+		return Note.builder()
+				//.id(noteDTO.getId())
+				.title(noteDTO.getTitle())
+				.text(noteDTO.getText())
+				.user(user)
+				.category(category)
+				.archived(false)
+				.build();
+	}
+	
+	public static Note toEntity(NoteDTO noteDTO,User user, Category category) {
+		return Note.builder()
+				.id(noteDTO.getId())
+				.title(noteDTO.getTitle())
+				.text(noteDTO.getText())
+				.user(user)
+				.category(category)
+			  //.archived(noteDTO.getArchived) TODO
+				.build();
+	}
+	
 }
