d396d8633bb699010bd0c70a41aed770bb0e72d8
matucson1 <115057949+matuteFanDeWandaNara@users.noreply.github.com>
2025-03-07 22:58:58 -0300

    feat(back)(service): finalize delete, archive, unarchive, findAllArchivedNotes and findAllUnarchivedNotes methods on NoteServiceImpl

--- backend/src/main/java/com/ensolvers/demo/service/NoteService.java
+++ backend/src/main/java/com/ensolvers/demo/service/NoteService.java
@@ -1,5 +1,31 @@
 package com.ensolvers.demo.service;
 
-public class NoteService {
+import java.util.List;
+
+import org.springframework.validation.annotation.Validated;
+
+import com.ensolvers.demo.dto.NoteDTO;
+
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.NonNull;
+
+@Validated
+public interface NoteService {
+	
+	NoteDTO createNote(@NonNull NoteDTO noteDTO);
+	
+	void deleteNote(@NonNull @PositiveOrZero Long id);
+	
+	NoteDTO updateNote(@NonNull NoteDTO noteDTO);
+	
+	List<NoteDTO> findAllArchivedNotes(@NonNull @PositiveOrZero Long id);
+	
+	List<NoteDTO> findAllUnArchivedNotes(@NonNull @PositiveOrZero Long id);
+	
+	List<NoteDTO> findAllNotes(@NonNull @PositiveOrZero Long id);
+	
+    NoteDTO archiveNote(@NonNull @PositiveOrZero Long id);
+    
+    NoteDTO unArchivedNote(@NonNull @PositiveOrZero Long id);
 
 }
--- backend/src/main/java/com/ensolvers/demo/service/impl/NoteServiceImpl.java
+++ backend/src/main/java/com/ensolvers/demo/service/impl/NoteServiceImpl.java
@@ -1,5 +1,95 @@
 package com.ensolvers.demo.service.impl;
 
-public class NoteServiceImpl {
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.ensolvers.demo.dto.NoteDTO;
+import com.ensolvers.demo.exception.ResourceNotFoundException;
+import com.ensolvers.demo.mapper.NoteMapper;
+import com.ensolvers.demo.model.Note;
+import com.ensolvers.demo.model.User;
+import com.ensolvers.demo.repository.NoteRepository;
+import com.ensolvers.demo.repository.UserRepository;
+import com.ensolvers.demo.service.NoteService;
+
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.NonNull;
+
+public class NoteServiceImpl implements NoteService {
+	
+	@Autowired
+	NoteRepository noteRepository;
+	
+	@Autowired
+	UserRepository userRepository;
+	
+
+	@Override
+	public NoteDTO createNote(@NonNull NoteDTO noteDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void deleteNote(@NonNull @PositiveOrZero Long id) {
+       Note note= getNoteByIdOrThrowException(id);
+       noteRepository.delete(note);
+	}
+
+	@Override
+	public NoteDTO updateNote(@NonNull NoteDTO noteDTO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<NoteDTO> findAllArchivedNotes(@NonNull @PositiveOrZero Long userid) {
+		User user = userRepository.findById(userid).orElseThrow(() -> new ResourceNotFoundException());
+		List<Note> archivedNotes = noteRepository.findByArchivedAndUser(true, user).orElse(Collections.emptyList());
+		
+		
+		return archivedNotes.stream()
+				.map(NoteMapper::toDTO)
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public List<NoteDTO> findAllUnArchivedNotes(@NonNull @PositiveOrZero Long userid) {
+		User user = userRepository.findById(userid).orElseThrow(() -> new ResourceNotFoundException());
+		List<Note> unArchivedNotes = noteRepository.findByArchivedAndUser(false, user).orElse(Collections.emptyList());
+		
+		
+		return unArchivedNotes.stream()
+				.map(NoteMapper::toDTO)
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public List<NoteDTO> findAllNotes(@NonNull @PositiveOrZero Long userid) {
+		return null;
+	}
+
+	@Override
+	public NoteDTO archiveNote(@NonNull @PositiveOrZero Long noteId) {
+		Note note = getNoteByIdOrThrowException(noteId);
+		note.setArchived(true);
+		Note savedNote= noteRepository.save(note);
+		return NoteMapper.toDTO(savedNote);
+	}
+
+	@Override
+	public NoteDTO unArchivedNote(@NonNull @PositiveOrZero Long noteId) {
+		Note note = getNoteByIdOrThrowException(noteId);
+		note.setArchived(false);
+		Note savedNote= noteRepository.save(note);
+		return NoteMapper.toDTO(savedNote);
+	}
+	
+	private Note getNoteByIdOrThrowException(Long noteId) {
+		return noteRepository.findById(noteId).orElseThrow(()-> new ResourceNotFoundException());
+	}
 
 }
